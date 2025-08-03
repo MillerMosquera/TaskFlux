fe752bcc0f3d34b1a43f0ae1f3d2bd2cf08968eb
Miller Mosquera <millerpalacios16@gmail.com>
2025-08-02 23:41:14 -0500

    feat: Implementa la autenticación y autorización JWT, incluidos los puntos finales de inicio de sesión y registro.

---
+++ backend/backend/src/main/java/org/miller/backend/security/Payload/LoginRequest.java
@@ -0,0 +1,1 @@
+package org.miller.backend.security.Payload;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;import lombok.NoArgsConstructor;@Data@Builder@AllArgsConstructor@NoArgsConstructorpublic class LoginRequest {    private String email;    private String passwordHash;}
---
+++ backend/backend/src/main/java/org/miller/backend/security/Payload/RegisterRequest.java
@@ -0,0 +1,1 @@
+package org.miller.backend.security.Payload;import lombok.AllArgsConstructor;import lombok.Builder;import lombok.Data;import lombok.NoArgsConstructor;@Data@Builder@AllArgsConstructor@NoArgsConstructorpublic class RegisterRequest {    private String email;    private String username;    private String passwordHash;}
