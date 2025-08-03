093f3bcdc0c2551388f770285a14c67425c10f94
Miller Mosquera <millerpalacios16@gmail.com>
2025-08-02 23:34:59 -0500

    feat: agrega interfaces de servicio para gestión de autenticación, comentarios, metas, notificaciones, espacios, subtareas, etiquetas, tareas y perfiles de usuario

---
+++ backend/backend/src/main/java/org/miller/backend/security/Auth/AuthService.java
@@ -0,0 +1,1 @@
+package org.miller.backend.security.Auth;import org.miller.backend.security.Payload.JwtResponse;import org.miller.backend.security.Payload.LoginRequest;import org.miller.backend.security.Payload.RegisterRequest;public interface AuthService {    JwtResponse register(RegisterRequest request);    JwtResponse login(LoginRequest request);}
